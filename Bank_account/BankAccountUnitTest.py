f9f2a504a4f55b0d662c32d0c0dfccda3f5aafb6
Bernadett Vegh <bernadettvegh@Bernadetts-MacBook-Pro.local>
2023-11-27 20:07:06 +0100

    update test

--- Bank_account/BankAccountUnitTest.py
+++ Bank_account/BankAccountUnitTest.py
@@ -1,5 +1,5 @@
 import unittest
-from BankAccount import Customer, CheckingAccount
+from BankAccount import Customer, CheckingAccount, Bank
 
 class TestCustomer(unittest.TestCase):
     def setUp(self):
@@ -49,6 +49,36 @@ class TestBankAccount(unittest.TestCase):
             "Account info for UTJ: Customer Information: Unit Test John, john@test.com, 567-1234"
         )
         self.assertEqual(self.account.account_holder_info(), expected_output)
+
+class TestBank(unittest.TestCase):
+    def setUp(self):
+        self.bank = Bank()
+
+        self.customer1 = Customer("John Johny", "johny@test.com", "444-1234")
+        self.customer2 = Customer("Tom Tommy", "tomy@test.com", "666-8901")
+
+    def test_add_customer(self): 
+        self.bank.add_customer(self.customer1)
+        self.assertIn(self.customer1, self.bank.customers)
+
+    def test_create_account(self):
+        self.bank.add_customer(self.customer2)
+        account = self.bank.create_account(self.customer2, initial_balance=1000.0)
+        self.assertIsInstance(account, CheckingAccount)
+
+    def test_print_customers(self):
+        self.bank.add_customer(self.customer1)
+        self.bank.add_customer(self.customer2)
+
+        with self.assertLogs() as log:
+            self.bank.print_customers()
+
+            expected_output = [
+                f"INFO:root:Customers in the bank:",
+                f"INFO:root:Customer Information: John Johny, johny@test.com, 444-1234",
+                f"INFO:root:Customer Information: Tom Tommy, tomy@test.com, 666-8901"
+            ]
+            self.assertEqual(log.output, expected_output)       
 
 if __name__ == "__main__":
     unittest.main()
